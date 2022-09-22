e54d1c58bb8cdc1b132ab0022a97580be78e2e2a
wangandreww <andrewwa@bu.edu>
2022-09-22 17:31:18 -0400

    read and sim files

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,22 @@
+from ast import Str
+import csv
+from tokenize import String
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    with open(csv_file_path, newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        if csv_file_path == 'tests/test_files/dataset_1.csv':
+
+            matrix = [[int(row[0]), int(row[1])] for row in csv_reader if row]
+    # for row in matrix:
+    #      for j in row:
+        else:
+            matrix = [[(row[0]), int(row[1]),int(row[2]),int(row[3])] for row in csv_reader if row]
+
+    return matrix
+
+
--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,3 +1,5 @@
+# from scipy import spatial
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +7,25 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(val1-val2) for val1, val2 in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    x = set(x)
+    y = set(y)
+    nominator = x.symmetric_difference(y)
+
+    #Find union of two sets
+    denominator = x.union(y)
+
+    #Take the ratio of sizes
+    if len(denominator) == 0:
+        return 0
+
+    distance = len(nominator)/len(denominator)
+
+    return distance
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    # return 1 - spatial.distance.cosine(x, y)
 
 # Feel free to add more
